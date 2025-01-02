d0c40b34ce2ee501f4157de0f6da6629fed2ae5f
KAMAL EZ-ZOUBIR <kamal98eth@gmail.com>
2025-01-02 02:03:48 +0100

    fix: change complain parameters

--- cpp-module-01/ex05/main.cpp
+++ cpp-module-01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kez-zoub <kez-zoub@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 23:19:36 by kez-zoub          #+#    #+#             */
-/*   Updated: 2024/11/07 19:48:22 by kez-zoub         ###   ########.fr       */
+/*   Updated: 2025/01/02 01:59:21 by kez-zoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ int	main()
 {
 	Harl	karen;
 
-	karen.complain("debug");
-	karen.complain("info");
-	karen.complain("warning");
-	karen.complain("error");
+	karen.complain("DEBUG");
+	karen.complain("INFO");
+	karen.complain("WARNING");
+	karen.complain("ERROR");
 	karen.complain("test");
 	
 	return (0);
