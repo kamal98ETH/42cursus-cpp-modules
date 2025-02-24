6ab0992826c16861b174aa9d4dc524a464c4774d
KAMAL EZ-ZOUBIR <kamal98eth@gmail.com>
2025-02-24 16:50:31 +0000

    fix: cpp01ex03 sigf

--- cpp-module-01/ex03/HumanB.cpp
+++ cpp-module-01/ex03/HumanB.cpp
@@ -6,13 +6,13 @@
 /*   By: kez-zoub <kez-zoub@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 00:18:51 by kez-zoub          #+#    #+#             */
-/*   Updated: 2025/01/02 01:30:48 by kez-zoub         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:15:45 by kez-zoub         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name(name) {};
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {};
 
 HumanB::~HumanB(void)
 {
@@ -26,5 +26,8 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack(void)
 {
-	std::cout << HumanB::name << " attacks with their " << HumanB::weapon->getType() << std::endl;
+	if (HumanB::weapon)
+		std::cout << HumanB::name << " attacks with their " << HumanB::weapon->getType() << std::endl;
+	else
+		std::cout << HumanB::name << " has NO weapon!" << std::endl;
 }
